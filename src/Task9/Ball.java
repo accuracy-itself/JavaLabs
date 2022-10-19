fcd9f90e00b115b759f37cb76a0edae3348797e7
Lena Stankevich <stankevich.lenkaa@mail.ru>
2022-10-19 15:00:25 +0300

    added task 9

---
+++ src/Task9/Ball.java
@@ -0,0 +1,11 @@
+package Task9;
+
+public class Ball {
+    public String Colour;
+    public int Weight;
+    public Ball(String colour, int weight)
+    {
+        this.Colour = colour;
+        Weight = weight; 
+    }
+}
